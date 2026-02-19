5f1e0d88345fb2cfbe2c58d72654978e49364607
danicorilo <otracuentadedany098@gmail.com>
2026-02-19 17:29:59 +0100

    carrerita

--- lab02-animacion/lab02-animacion.py
+++ lab02-animacion/lab02-animacion.py
@@ -1,5 +1,5 @@
 import arcade
-from math import sin
+from math import sin, tan, tanh, sinh, log, e, pi, cos
 from lab02_draw import dibujar_coche
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
@@ -11,13 +11,21 @@ class MiJuego(arcade.Window):
 
 
         self.coordsX = 50
+        self.coordsX2= 40
+        self.coordsX3=pi
     
     def on_draw(self):
 
         self.clear()
         arcade.draw_lrbt_rectangle_filled(0, 800, 0, 200, arcade.color.BLACK_OLIVE)
-        dibujar_coche(sin(self.coordsX)*300,250)
+        dibujar_coche((tan(self.coordsX))*200,250, arcade.color.AFRICAN_VIOLET)
+        dibujar_coche(tan(self.coordsX)*500, 190, arcade.color.ALABAMA_CRIMSON)
+        dibujar_coche((tan((self.coordsX))*sin(self.coordsX))*500,130, arcade.color.YELLOW)
+        dibujar_coche(tan(self.coordsX3)*20,100,arcade.color.ALMOND)
         self.coordsX+=0.01
+        self.coordsX2+=2
+        self.coordsX3+=0.012
+        
 
         # Poner aquí el código del dibujo
         # Cambiar la posición y/o tamaño del dibujo para crear animación
--- lab02-animacion/lab02_draw.py
+++ lab02-animacion/lab02_draw.py
@@ -1,9 +1,9 @@
 
 import arcade
 
-def dibujar_coche (x,y):
-    arcade.draw_rect_filled(arcade.XYWH(x, y+67, 140, 80), arcade.color.PURPLE_MOUNTAIN_MAJESTY)
-    arcade.draw_rect_filled(arcade.XYWH(x, y, 300, 80), arcade.color.PURPLE_MOUNTAIN_MAJESTY)
+def dibujar_coche (x,y,color):
+    arcade.draw_rect_filled(arcade.XYWH(x, y+67, 140, 80), color)
+    arcade.draw_rect_filled(arcade.XYWH(x, y, 300, 80), color)
     arcade.draw_circle_filled(x-60,y-43,40, arcade.color.BLACK_LEATHER_JACKET)
     arcade.draw_circle_filled(x+60,y-43,40, arcade.color.BLACK_LEATHER_JACKET)
 
